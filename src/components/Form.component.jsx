42f0506ca44e04978ab11eec92bbbffd46850b26
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-05 18:08:35 +0500

    Display Notes and added add note functionality

--- src/components/Form.component.jsx
+++ src/components/Form.component.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import NotesList from "./NotesList.component";
 
-const Form = () => (
-    <div className="container">
-        <div className="my-3">
-            <label for="exampleFormControlInput1" className="form-label">Note Title</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Add note title"/>
+
+const Form = () => {
+    const [notes, setNotes] = useState([]);
+    const [noteTitle, setNoteTitle] = useState('');
+    const [noteDesc, setNoteDesc] = useState('');
+    const handleClick = () => {
+        const notesObj = {
+            title: noteTitle,
+            desc: noteDesc,
+            id: Math.random() + noteTitle
+        }
+        setNotes([...notes, notesObj]);
+        // console.log(notes);
+    }
+
+
+    return (
+
+        <div className="container">
+            <div className="my-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Note Title</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" value={noteTitle} placeholder={noteTitle} onChange={e => setNoteTitle(e.target.value)} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">Note Details</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={noteDesc} placeholder={noteDesc} onChange={e => setNoteDesc(e.target.value)}></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleClick}>Add Note</button>
+            <hr />
+            <h1>Notes:</h1>
+            <NotesList notes={notes} setNotes={setNotes} />
         </div>
-        <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">Note Details</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Add note details"></textarea>
-        </div>
-        <button className="btn btn-primary">Add Note</button>
-    </div>
-)
+        
+    )
+}
 
 export default Form;
