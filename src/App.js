0c2be18a77e3dabb45ff01fb8afe248205bce91a
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-05 18:06:48 +0500

    Display Notes in UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
+import React from 'react';
 import Header from './components/Header.component';
 import Form from './components/Form.component';
+// import NotesList from './components/NotesList.component';
 
 function App() {
+
   return (
     <>
-      <Header/>
-      <Form/>
+      <Header />
+      <Form />
     </>
-  );
+  )
+
 }
 
 export default App;
