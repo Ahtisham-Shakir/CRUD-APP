e489f92b3a620dacbe67c296cceff11dd5da78cd
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-06 17:14:46 +0500

    Adding Delete Note Functionality

--- src/components/Form.component.jsx
+++ src/components/Form.component.jsx
@@ -6,6 +6,18 @@ const Form = () => {
     const [notes, setNotes] = useState([]);
     const [noteTitle, setNoteTitle] = useState('');
     const [noteDesc, setNoteDesc] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+    const [noteId, setNoteId] = useState('');
+
+    // Reset Function
+    const handleReset = () => {
+        setIsEditing(false);
+        setNoteTitle('');
+        setNoteDesc('');
+        setNoteId('');
+    }
+
+    // Add Note Function
     const handleClick = () => {
         const notesObj = {
             title: noteTitle,
@@ -13,27 +25,40 @@ const Form = () => {
             id: Math.random() + noteTitle
         }
         setNotes([...notes, notesObj]);
-        // console.log(notes);
+
+        handleReset();
+    }
+
+    const handleUpdate =()=>{
+       const updatedArr= notes.map((element)=>{
+            if(noteId === element.id){
+                element.title = noteTitle;
+                element.desc = noteDesc;
+            }
+            return element;
+        })
+        setNotes(updatedArr);
+        handleReset();
     }
 
 
     return (
-
-        <div className="container">
-            <div className="my-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Note Title</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" value={noteTitle} placeholder={noteTitle} onChange={e => setNoteTitle(e.target.value)} />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">Note Details</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={noteDesc} placeholder={noteDesc} onChange={e => setNoteDesc(e.target.value)}></textarea>
-            </div>
-            <button className="btn btn-primary" onClick={handleClick}>Add Note</button>
-            <hr />
-            <h1>Notes:</h1>
-            <NotesList notes={notes} setNotes={setNotes} />
-        </div>
         
+            <div className="container">
+                <div className="my-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Note Title</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" value={noteTitle} placeholder={noteTitle} onChange={e => setNoteTitle(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Note Details</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={noteDesc} placeholder={noteDesc} onChange={e => setNoteDesc(e.target.value)}></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={isEditing? handleUpdate: handleClick}>{isEditing ? 'Update Note' : 'Add Note'}</button>
+                {isEditing && <button className="btn btn-primary mx-2" onClick={handleReset}>Cancel</button>}
+                <hr />
+                <h1>Notes:</h1>
+                <NotesList notes={notes} setNotes={setNotes} setIsEditing={setIsEditing} setNoteTitle={setNoteTitle} setNoteDesc={setNoteDesc} setNoteId={setNoteId}/>
+            </div>
     )
 }
 
--- src/components/NotesList.component.jsx
+++ src/components/NotesList.component.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-const NotesList = ({ notes, setNotes }) => {
+const NotesList = ({ notes, setNotes, setIsEditing, setNoteTitle, setNoteDesc, setNoteId }) => {
+
+    // Function to Delete Note
     const handleDelete = (id) => {
         const updatedNotes = notes.filter(note => note.id !== id);
         setNotes(updatedNotes);
     }
+
+    // Function on Edit Button
+    const handleEdit = (id) => {
+        setIsEditing(true);
+        for (let index = 0; index < notes.length; index++) {
+            const note = notes[index];
+            if(note.id === id){
+                setNoteId(note.id);
+                setNoteTitle(note.title);
+                setNoteDesc(note.desc);
+                break;
+            }
+            
+        }
+    }
+
     return (
-        <div style={{ display: "flex" , flexWrap:'wrap'}}>
+        <div style={{ display: "flex", flexWrap: 'wrap' }}>
             {notes.map((element) => (
 
                 <div key={element.id} className="card" style={{ width: '18rem', margin: '5px' }}>
@@ -14,6 +32,7 @@ const NotesList = ({ notes, setNotes }) => {
                         <h5 className="card-title">{element.title}</h5>
                         <p className="card-text">{element.desc}</p>
                         <button className="btn btn-primary" onClick={() => handleDelete(element.id)}>Delete</button>
+                        <button className="btn btn-primary mx-2" onClick={() => handleEdit(element.id)}>Edit</button>
                     </div>
                 </div>
             )
